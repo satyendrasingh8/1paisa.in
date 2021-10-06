60e4ef891713f536a8b2eb1061ea49105be75943
satyendrasingh8 <satyendrasinghsss7398@gmai.com>
2021-10-06 13:22:15 +0530

    header page initial commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Header page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<header><nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)">1paisa</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Team</a>
+        </li>
+        <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Loan</a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Mobile Loan</a></li>
+    <li><a class="dropdown-item" href="#">Small Loan</a></li>
+    <li><a class="dropdown-item" href="#">Micro Loan</a></li>
+    <li><a class="dropdown-item" href="#">Shop Loan</a></li>
+    <li><a class="dropdown-item" href="#">House Loan</a></li>
+    <li><a class="dropdown-item" href="#">Personal Loan</a></li>
+    <li><a class="dropdown-item" href="#">Vehicle Loan</a></li>
+    <li><a class="dropdown-item" href="#">Group Loan</a></li>
+
+  </ul>
+</li>
+<li><a class="dropdown-item" href="#">Contact</a></li>
+      </ul>
+
+
+      <div class="d-flex align-items-center">
+      <li class="nav-item dropdown">
+  <a class="nav-link dropdown-toggle text-secondary" href="#" role="button" data-bs-toggle="dropdown">Login</a>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Login</a></li>
+    <li><a class="dropdown-item" href="#">Register</a></li>
+
+  </ul>
+</li>
+<li class="nav-item">
+          <a class="nav-link text-secondary" href="javascript:void(0)">Contact</a>
+        </li>
+</div>
+    </div>
+  </div>
+</nav>
+ </header>
+
+<div class="container-fluid mt-3">
+  <h3>Navbar Forms</h3>
+  <p>You can also include forms inside the navigation bar.</p>
+</div>
+
+</body>
+</html>
+
+
