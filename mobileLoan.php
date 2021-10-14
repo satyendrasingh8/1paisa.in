3317a1f1504e4750443330fa33b47cc1b23cc5a1
satyendrasingh8 <satyendrasinghsss7398@gmai.com>
2021-10-14 06:59:18 +0530

    all pages

--- mobileLoan.php
+++ mobileLoan.php
@@ -42,8 +42,8 @@ Wondering how to buy a mobile phone on EMI with 1paisa.com? We have a step by st
   <img  src="https://topflightapps.com/wp-content/uploads/2020/07/Mobile-loan-app-hero-image.jpg" 
   class="card-img-top" style="height: 330px;" alt="mobile loan">
   <div class="card-body" style="width:90%; text-align:center;">
-    <h3 class="card-title animate__animated animate__pulse" style="text-align: center; padding-top:20px;">
-    <span class="badge bg-secondary" >3 Easy Steps to buy your new</br> phone!</span></h3>
+    <h2 class="card-title animate__animated animate__pulse" style="text-align: center; padding-top:20px;">
+    <span class="badge bg-dark" >3 Easy Steps to buy your new</br> phone!</span></h2>
     <p class="card-text">
     
 
@@ -135,29 +135,61 @@ An Aadhaar card is mandatory for NIRA smartphone loan application and to buy a m
 
 
 
+<!--   Eligibility criteria and Documents required   -->
 
 
+<div class="card-group">
 
-<div class="card mb-3" style="max-width: 100%;">
-  <div class="row g-0">
-    <div class="col-md-6">
-      <img src="https://assets.fullertonindia.com/sites/default/files/instant_PL.png" 
-      class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-6">
-      <div class="card-body">
-        <h5 class="card-title">Loan Services</h5>
-        <p class="card-text">This is  how all this mistaken idea of denouncing pleasure and praising pain was born and 
-I will give you a complete account of the system, and expound the actual teachings of the great explorer of
- the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because
-  it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that
-   are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because
-    it is pain, but because occasioa wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
+<div class="row">
+<div class="card col-md-6 sat-card-hover" style="text-align:center;">
+<i class="fa fa-users" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Eligibility Criteria</h5>
+    <div class="card-body">
+      
+      <p class="card-text">
+      <ul style="text-align:start;">
+<li>   Must be a full-time salaried employee with a minimum take-home salary of ₹ 30,000/month
+
+OR
+Must be a self-employed professional with an income of at least ₹ 30,000/month.
+(Only certain professionals like doctors, lawyers, or business owners qualify) </li>
+<li> Must be above 23 years and below 55 years of age </li>
+<li> Must be a resident of one of the following cities:  </li>
+      </ul>  
+    
+</p>
+      <button type="button" class="btn btn-outline-danger">View More</button>
     </div>
   </div>
+  
+<div class="card col-md-6 sat-card-hover" style="text-align:center;">
+<i class="fa fa-trophy" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Documents Required</h5>
+    <div class="card-body">
+      
+      <p class="card-text">
+      <div class="row" style="text-align:start;">  
+        <div><i class="fas fa-id-card"></i>
+      <b>Pan Card Number</b></div>
+   
+      <div><i class="fas fa-camera"></i> <b> Professional Selfie </b>
+(To be taken on the MoneyTap App)</div>
+
+<div> <i class="fas fa-home"></i> <b>
+Address Proof </b>
+(Valid Driving License / Valid Passport / Aadhar Card)
+
+<div> <i class="fas fa-fingerprint"></i> <b>
+ID Proof</b>
+(Valid Driving License / Valid Passport / Voter’s ID / Aadhar Card / Pan Card) </div> </div> </p>
+<button type="button" class="btn btn-outline-danger">View More</button>
+
+    </div>
+  </div>
+  
+
+  </div> 
 </div>
+
+
 
 
 <!--   Why People choose us   -->
@@ -171,7 +203,7 @@ I will give you a complete account of the system, and expound the actual teachin
 </div>
 
 <div class="row">
-<div class="card col-md-4" style="text-align:center;">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
 <i class="fa fa-users" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Dedicated Specialists</h5>
     <div class="card-body">
       
@@ -180,7 +212,7 @@ I will give you a complete account of the system, and expound the actual teachin
     </div>
   </div>
   
-<div class="card col-md-4" style="text-align:center;">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
 <i class="fa fa-trophy" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Success Stories Rating</h5>
     <div class="card-body">
       
@@ -189,7 +221,7 @@ I will give you a complete account of the system, and expound the actual teachin
     </div>
   </div>
   
-<div class="card col-md-4" style="text-align:center;">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
 
 <i class='fas fa-hand-holding-usd' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Why Choose Us!</h5>
     <div class="card-body">
@@ -204,7 +236,130 @@ I will give you a complete account of the system, and expound the actual teachin
 
 
 
+<!--   loan services    -->
+
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <img src="https://assets.fullertonindia.com/sites/default/files/instant_PL.png" 
+      class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+      <h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">Guaranteed customer happiness </h1>
+        <p class="card-text"><div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front" >
+    <img src="https://www.homecredit.co.in/sites/default/files/2021-06/customer_img01_2.jpg" />
+     
+
+    </div>
+    <div class="flip-box-back">
+    <img src="https://www.homecredit.co.in/sites/default/files/2021-06/testim-img1.jpg" />
+    
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 <!--   We are here to help you     -->
+
+
+
+
+
+
+<!--  
+<div class="card-group" >
+<div class="card col">
+
+<div class="row">
+<h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">We are here to help you </h1>
+</div>
+
+<div class="row">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;" >
+<i class="fa fa-landmark" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Apply For Loan</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Looking to buy a mobile loan? then apply for loan now.</p>
+      <button type="button" class="btn btn-outline-danger">Get Appointment</button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-address-card" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Contact us at</h5>
+    <div class="card-body">
+      
+      <p class="card-text">rishi78srivastava@gmail.com</p>
+      <button type="button" class="btn btn-outline-danger">Contact us </button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+
+<i class='fas fa-comments' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Talk to Advisor</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Need to loan advise? Talk to our Loan advisors.</p>
+      <button type="button" class="btn btn-outline-danger">Meet The Advisor</button>
+    </div>
+  </div>
+
+  </div> </div>
+</div>
+    -->
+
+    
+<div class="card-group">
+<div class="card col">
+
+<div class="row">
+<h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">We are here to help you </h1>
+</div>
+
+<div class="row">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-landmark" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Apply For Loan</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Looking to buy a mobile loan? then apply for loan now.</p>
+      <button type="button" class="btn btn-outline-danger">Get Appointment</button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-address-card" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Contact us at</h5>
+    <div class="card-body">
+      
+    <p class="card-text">rishi78srivastava@gmail.com</p>
+      <button type="button" class="btn btn-outline-danger">Contact us </button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+
+<i class='fas fa-comments' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Talk to Advisor</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Need to loan advise? Talk to our Loan advisors. We are here to help you 24*7.
+</p>
+      <button type="button" class="btn btn-outline-danger">Meet The Advisor</button>
+    </div>
+  </div>
+
+  </div> </div>
+</div>
+
+
+
+
+
 
 
 
--- shopLoan.php
+++ shopLoan.php
@@ -1,16 +1,370 @@
 <?php include './layout/header.php';
 ?>
-<div class="sat-container" > <div>
-    <h2>Shop Loan</h2>
+<!---- Top slider   -->
+<div id="carouselExampleInterval"  class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="900" >
+      <img src="https://www.bajajfinserv.in/Best_Oppo_Phones_Under_Rs5000_img1_Oppo-790x345-min.jpg" style="height: 300px;" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://www.writersevoke.com/wp-content/uploads/2019/10/Best-Personal-Loan-Apps-in-India.png" style="height: 300px;"  class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="1600">
+      <img src="https://www.cashoverflow.in/wp-content/uploads/2020/07/best-instant-loan-app-in-India.png" style="height: 300px;" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<!--  top text section  -->
+
+<div class="sat-container" > <div class="sat-top-section">
+<h1 class="animate__animated animate__fadeIn">Mobile Loan</h1>
+<div class="animate__animated animate__fadeIn">
     <p>Get Instant Mobile Loan up to ₹1 Lakhs Easily at 1paisa.com.
 <b>3 easy and simple steps to buy a new phone!</b> 
 Wondering how to buy a mobile phone on EMI with 1paisa.com? We have a step by step guide ready for you. All you need to do to buy your mobile phone on EMI is to follow the steps below:
 </p>
 </div>
+<button type="button" class="btn btn-primary">Apply Now</button>
+</div>
 
-<button type="button" class="btn btn-outline-warning">Apply Now</button>
+<!--  3 easy steps to buy mobile  -->
+
+<div class="card mb-3">
+  <img  src="https://topflightapps.com/wp-content/uploads/2020/07/Mobile-loan-app-hero-image.jpg" 
+  class="card-img-top" style="height: 330px;" alt="mobile loan">
+  <div class="card-body" style="width:90%; text-align:center;">
+    <h2 class="card-title animate__animated animate__pulse" style="text-align: center; padding-top:20px;">
+    <span class="badge bg-dark" >3 Easy Steps to buy your new</br> phone!</span></h2>
+    <p class="card-text">
+    
+
+Wondering how to buy a mobile phone on EMI with <b>1paisa - Finance company? </b> We have a step by step guide ready for you.
+ All you need to do to buy your mobile phone on EMI is to follow the steps below:
+
+<div class="list-group" >
+  
+     <button type="button" class="btn btn-primary" >1. Go to 1paisa.in website</button>
+
+  
+</br>
+  <button type="button" class="btn btn-primary">2. Fill in your details and upload the required documents</button>
+ 
+  </br>
+  <button type="button" class="btn btn-primary">3. Set up our inbuilt auto-debit system and avail your personal loan</button>
+  </br>
+      
+  <button type="button" class="btn btn-danger">Congratulations! You can now buy the phone of your choice!</button>
+</div>
+</br>
+
+  </div>
 </div>
 
 
+
+<!--  How it works 1paisa  -->
+
+<div class="card">
+  <h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">How it works </h1>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://images.financialexpress.com/2018/03/iphonex-reuters-6.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title animate__animated animate__fadeIn"> Elibility Criteria</h5>
+        <p class="animate__animated animate__fadeIn">survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+           unchanged. It was popularised in the 1960s with the release of Letraset sheets containing </p>
+
+        <h5 class="card-title animate__animated animate__fadeIn">  Documents Required</h5>
+        <p class="animate__animated animate__fadeIn">survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+           unchanged. It was popularised in the 1960s with the release of Letraset sheets containing </p>
+
+
+        <h5 class="card-title animate__animated animate__fadeIn"> Fees & Charges</h5>
+        <p class="animate__animated animate__fadeIn">survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+           unchanged. It was popularised in the 1960s with the release of Letraset sheets containing </p>
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<!--   Apply for mobile phones on EMI   -->
+
+<div class="card mb-3 bg-dark text-white" style="width:100%;">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <div class="card-body">
+        <h1 class="card-title animate__animated animate__fadeIn">Apply for mobile phones on EMI</h1>
+        <p class="card-text animate__animated animate__fadeIn">You should have a graduate degree.
+It’s important to hold a college degree to finance your mobile on EMI from NIRA. A college degree builds on your credibility and credentials for a smartphone loan application.
+</br></br>
+Your monthly income should be Rs. 20,000 or more ​
+To be eligible to finance your mobile phone on EMI with a debit card, you need to display an ability to be able to afford the EMI repayment. For this reason, your salary plays a crucial role in your smartphone loan application.
+</br></br>
+You should be employed for at least 6 months.
+While it isn’t necessary to have many years of work experience, a continuous and stable employment of minimum 6 months increases your chances of getting qualified for a smartphone loan to help you buy mobile phones on EMI.
+</br></br>
+You have an Aadhaar card.
+An Aadhaar card is mandatory for NIRA smartphone loan application and to buy a mobile emi on aadhar card. This will help speed up the process of completion of verification and KYC.
+
+</p>
+        <p class="card-text"><button type="button" class="btn btn-danger">Check Eligibility criteria</button> </p>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <img src="https://www.gizbot.com/images/2019-02/vivo-u1_155107538060.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+
+  </div>
+</div>
+
+
+
+<!--   Eligibility criteria and Documents required   -->
+
+
+<div class="card-group">
+
+<div class="row">
+<div class="card col-md-6 sat-card-hover" style="text-align:center;">
+<i class="fa fa-users" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Eligibility Criteria</h5>
+    <div class="card-body">
+      
+      <p class="card-text">
+      <ul style="text-align:start;">
+<li>   Must be a full-time salaried employee with a minimum take-home salary of ₹ 30,000/month
+
+OR
+Must be a self-employed professional with an income of at least ₹ 30,000/month.
+(Only certain professionals like doctors, lawyers, or business owners qualify) </li>
+<li> Must be above 23 years and below 55 years of age </li>
+<li> Must be a resident of one of the following cities:  </li>
+      </ul>  
+    
+</p>
+      <button type="button" class="btn btn-outline-danger">View More</button>
+    </div>
+  </div>
+  
+<div class="card col-md-6 sat-card-hover" style="text-align:center;">
+<i class="fa fa-trophy" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Documents Required</h5>
+    <div class="card-body">
+      
+      <p class="card-text">
+      <div class="row" style="text-align:start;">  
+        <div><i class="fas fa-id-card"></i>
+      <b>Pan Card Number</b></div>
+   
+      <div><i class="fas fa-camera"></i> <b> Professional Selfie </b>
+(To be taken on the MoneyTap App)</div>
+
+<div> <i class="fas fa-home"></i> <b>
+Address Proof </b>
+(Valid Driving License / Valid Passport / Aadhar Card)
+
+<div> <i class="fas fa-fingerprint"></i> <b>
+ID Proof</b>
+(Valid Driving License / Valid Passport / Voter’s ID / Aadhar Card / Pan Card) </div> </div> </p>
+<button type="button" class="btn btn-outline-danger">View More</button>
+
+    </div>
+  </div>
+  
+
+  </div> 
+</div>
+
+
+
+
+<!--   Why People choose us   -->
+
+
+<div class="card-group">
+<div class="card col">
+
+<div class="row">
+<h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">Why People choose Us! </h1>
+</div>
+
+<div class="row">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-users" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Dedicated Specialists</h5>
+    <div class="card-body">
+      
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="btn btn-outline-danger">Meet The Team</button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-trophy" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Success Stories Rating</h5>
+    <div class="card-body">
+      
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="btn btn-outline-danger">View Client Rating </button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+
+<i class='fas fa-hand-holding-usd' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Why Choose Us!</h5>
+    <div class="card-body">
+      
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button type="button" class="btn btn-outline-danger">Why Choose Us</button>
+    </div>
+  </div>
+
+  </div> </div>
+</div>
+
+
+
+<!--   loan services    -->
+
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <img src="https://assets.fullertonindia.com/sites/default/files/instant_PL.png" 
+      class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+      <h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">Guaranteed customer happiness </h1>
+        <p class="card-text"><div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front" >
+    <img src="https://www.homecredit.co.in/sites/default/files/2021-06/customer_img01_2.jpg" />
+     
+
+    </div>
+    <div class="flip-box-back">
+    <img src="https://www.homecredit.co.in/sites/default/files/2021-06/testim-img1.jpg" />
+    
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!--   We are here to help you     -->
+
+
+
+
+
+
+<!--  
+<div class="card-group" >
+<div class="card col">
+
+<div class="row">
+<h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">We are here to help you </h1>
+</div>
+
+<div class="row">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;" >
+<i class="fa fa-landmark" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Apply For Loan</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Looking to buy a mobile loan? then apply for loan now.</p>
+      <button type="button" class="btn btn-outline-danger">Get Appointment</button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-address-card" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Contact us at</h5>
+    <div class="card-body">
+      
+      <p class="card-text">rishi78srivastava@gmail.com</p>
+      <button type="button" class="btn btn-outline-danger">Contact us </button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+
+<i class='fas fa-comments' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Talk to Advisor</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Need to loan advise? Talk to our Loan advisors.</p>
+      <button type="button" class="btn btn-outline-danger">Meet The Advisor</button>
+    </div>
+  </div>
+
+  </div> </div>
+</div>
+    -->
+
+    
+<div class="card-group">
+<div class="card col">
+
+<div class="row">
+<h1 class="card-header text-center bg-dark text-light animate__animated animate__fadeIn">We are here to help you </h1>
+</div>
+
+<div class="row">
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-landmark" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Apply For Loan</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Looking to buy a mobile loan? then apply for loan now.</p>
+      <button type="button" class="btn btn-outline-danger">Get Appointment</button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+<i class="fa fa-address-card" style="font-size:60px; padding:10px;"></i> <h5 class="card-title">Contact us at</h5>
+    <div class="card-body">
+      
+    <p class="card-text">rishi78srivastava@gmail.com</p>
+      <button type="button" class="btn btn-outline-danger">Contact us </button>
+    </div>
+  </div>
+  
+<div class="card col-md-4 sat-card-hover" style="text-align:center;">
+
+<i class='fas fa-comments' style='font-size:60px; padding:10px;'></i> <h5 class="card-title">Talk to Advisor</h5>
+    <div class="card-body">
+      
+      <p class="card-text">Need to loan advise? Talk to our Loan advisors. We are here to help you 24*7.
+</p>
+      <button type="button" class="btn btn-outline-danger">Meet The Advisor</button>
+    </div>
+  </div>
+
+  </div> </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
 <?php include './layout/footer.php';
 ?>
