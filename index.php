0fbdf2264604b9cacb03ac44e3bacb8ac33d2ac2
satyendrasingh8 <satyendrasinghsss7398@gmai.com>
2021-11-22 10:22:35 +0530

    initialize about page

--- index.php
+++ index.php
@@ -2,7 +2,153 @@
 <?php include './layout/header.php';
 ?>
 
-
+<div id="body">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">Mobile Loan <br>
+                                </h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">Get Instant Mobile Loan up to ₹1 Lakhs Easily at 1paisa.in
+                                </p>
+                                <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">House Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">Fulfill the dream of owning your dream home at lowest interest rates @8.7 Apply Today! Online at 1paisa.in</p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">Personal Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">Apply online for Personal Loan & get instant loan at 1paisa.in. <br>
+                                    A need for a safe and comfortable lifestyle, a need to have a safety net to tackle any unplanned/immediate requirement.
+                                </p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">Vehical Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">1paisa.in offers you the best vehicle loans worth up to 100% of the on-road price <br> of the car you choose to buy, along with attractive interest rates and tenure of up to five years.</p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".1">Micro Small Loan <br>
+                                </h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".5">Apply online for Micro Small Loan & get instant loan at 1paisa.in. <br>
+                                    A need for a safe and comfortable lifestyle, a need to have a safety net to tackle any unplanned/immediate requirement.
+                                </p>
+                                <a data-duration-in=".3" data-animation-in="fadeInUp" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">Small Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">Apply online for Small Loan & get instant loan at 1paisa.in. <br>
+                                    A need for a safe and comfortable lifestyle, a need to have a safety net to tackle any unplanned/immediate requirement.</p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">Shop Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">Apply online for Shop Loan & get instant loan at 1paisa.in. <br>
+                                    A need for a safe and comfortable lifestyle, a need to have a safety net to tackle any unplanned/immediate requirement.
+                                </p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="slider-item th-fullpage hero-area" style="background-image: url(images/slider/slider-bg-2.jpg);">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h1 data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".1">Group Loan</h1>
+                                <p data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".5">Apply online for Group Loan & get instant loan at 1paisa.in. <br>
+                                    A need for a safe and comfortable lifestyle, a need to have a safety net to tackle any unplanned/immediate requirement.</p>
+                                <a data-duration-in=".3" data-animation-in="fadeInDown" data-delay-in=".8" class="btn btn-main" href="#">Explore Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    <!-- Main jQuery -->
+    <script src="jquery/jquery.min.js"></script>
+    <!-- Bootstrap4 -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Parallax -->
+    <!-- Owl Carousel -->
+    <!-- <script src="slick/slick.min.js"></script> -->
+    <!-- filter -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.carousel').carousel({
+                interval: 2000
+            })
+        });
+    </script>
+</div>
 
 <?php include './layout/footer.php';
 ?>
